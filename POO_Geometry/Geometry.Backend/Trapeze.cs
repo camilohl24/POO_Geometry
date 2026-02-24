1b1a58f9acae436d66ea796e0c20f530a07d54e3
camilohl24 <juancamilohernandezhl@gmail.com>
2026-02-24 15:17:31 -0500

    Add new figure tapeze

---
+++ POO_Geometry/Geometry.Backend/Trapeze.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+
+namespace Geometry.Backend;
+
+public class Trapeze : Triangle
+{
+    private double _d;
+    public Trapeze(string name, double a, double b, double c, double h, double d) : base(name, a, b, c, h)
+    {
+        D = d;
+    }
+
+    public double D 
+    {
+        get => _d;
+        set => _d = ValidateD(value);
+    }
+    public override double GetArea() => ((B + D) * H) / 2 ;
+
+
+    public override double GetPerimeter() => A + B + C + D;
+    private double ValidateD(double d)
+    {
+        if (double.IsNaN(d) || double.IsInfinity(d) || d <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(d), $"Side {d} must be greater than zero,real and positive.");
+        }
+        return d;
+    }
+}
